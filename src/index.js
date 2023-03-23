4bfb2341abd3787f9d2824b27fa3397f1f81b57b
Arttu Sulkonen <arttusulkonen@Arttus-MacBook-Pro.local>
2023-03-23 15:00:47 +0200

    Add regular expression to verify the correctness of the IP address

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { validateIp } from './helpers';
 
 const searchButton = document.querySelector('.search-bar__btn');
 const searchInput = document.querySelector('.search-bar__input');
@@ -9,15 +10,11 @@ searchInput.addEventListener('keydown', handleKey);
 function getData() {
   const ipAddress = searchInput.value;
   const url = `https://geo.ipify.org/api/v2/country?apiKey=${process.env.API_KEY}&ipAddress=${ipAddress}`;
-
-  axios
-    .get(url)
-    .then(function (response) {
+  if (validateIp(ipAddress)) {
+    axios.get(url).then(function (response) {
       console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
     });
+  }
 }
 
 function handleKey(e) {
