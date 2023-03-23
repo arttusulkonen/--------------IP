b6cc0f0f105860bf186e9f1e803a79a6e527467f
Arttu Sulkonen <arttusulkonen@Arttus-MacBook-Pro.local>
2023-03-23 14:45:02 +0200

    create ip query

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+import axios from 'axios';
 
+const searchButton = document.querySelector('.search-bar__btn');
+const searchInput = document.querySelector('.search-bar__input');
+
+searchButton.addEventListener('click', getData);
+searchInput.addEventListener('keydown', handleKey);
+
+function getData() {
+  const ipAddress = searchInput.value;
+  const url = `https://geo.ipify.org/api/v2/country?apiKey=${process.env.API_KEY}&ipAddress=${ipAddress}`;
+
+  axios
+    .get(url)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function handleKey(e) {
+  if (e.key === 'Enter') {
+    getData();
+  }
+}
